e18e0dc1db7ba5d7c491100138dae9eeb7d78295
Tomasz Gadek <gadektomek@gmail.com>
2020-04-30 09:35:46 +0200

    init project

---
+++ src/main/java/pl/edu/pwsztar/controller/ChessApiController.java
@@ -0,0 +1,28 @@
+package pl.edu.pwsztar.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import pl.edu.pwsztar.domain.dto.FigureMoveDto;
+
+@Controller
+@RequestMapping(value="/api")
+public class ChessApiController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ChessApiController.class);
+
+    @CrossOrigin
+    @PostMapping(value = "/chess/is-correct-move")
+    public ResponseEntity<Boolean> isCorrectMove(@RequestBody FigureMoveDto figureMoveDto) {
+        LOGGER.info("*** move details : {}", figureMoveDto);
+
+        // TODO: true = ruch dozwolony (figura moze przemiescic sie z punktu start do punktu destination)
+        // TODO: false = ruch zabroniony (figura nie moze przemiescic sie z punktu start do do punktu destination)
+        return ResponseEntity.ok(true);     // TODO: teraz figura moze przemieszczac sie do dowolnego pola
+    }
+}
